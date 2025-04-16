dfbbe76b2a1a4142cde2b2f4c4d2594e57232d6f
Filipe Fraqueiro <fraqueirofilipe@gmail.com>
2025-04-16 16:29:09 +0200

    updates

--- programming/python/totp.py
+++ programming/python/totp.py
@@ -1,187 +1,63 @@
-import requests
-import json
+import hmac
+import base64
 import time
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-URL_BASE = "https://api.weatherapi.com/v1/"
-API_KEY = os.getenv("WEATHERAPI_KEY")
-# print(API_KEY)
-
-# Current weather
-# current
-# Forecast
-# forecast
-# Search or Autocomplete
-# search
-# History
-# history
-# Time Zone
-# timezone
-# Sports
-# sports
-# Astronomy
-# astronomy
-# IP Lookup
-# ip
-
-# request type
-# json or xml
-
-# air quality
-# aqi = "yes" or "no"
-
-# date = "yyyy-MM-dd"
-
-# location
-# q
-# Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name
+# otpauth://totp/[LABEL]?secret=[SECRET]&issuer=[ISSUER]&algorithm=SHA1&digits=6&period=30
+# secret is BASE32
+# make a qrcode of the above
 #
 #
 #
-def current(req_format:str="json", location:str="Lisboa", aqi:str="yes")->dict:
-	"""
-    Get the current weather for the location provided
-	req_format: The output format
-	location: The location to get the weather for
-	aqi: If the AQI (Air Quality Index) should be returned or not
+def int_to_bytestring(i, padding=8):
     """
-		
-	req_type = "current"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
-
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-	params["aqi"] = aqi
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
+    Turns an integer to the OATH specified
+    bytestring, which is fed to the HMAC
+    along with the secret
+    """
+    result = bytearray()
+    while i != 0:
+        result.append(i & 0xFF)
+        i >>= 8
+    # It's necessary to convert the final result from bytearray to bytes
+    # because the hmac functions in python 2.6 and 3.3 don't work with
+    # bytearray
+    result = bytes(bytearray(reversed(result)).rjust(padding, b'\0'))
+    # print(result)
+    return result
 #
 #
 #
-def forecast(req_format="json", location="Lisboa", aqi="yes", days=1, alerts="yes"):
-	req_type = "forecast"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
+def totp(key):
+    time_ = time.time()
+    time_ = time_/30
+    time_ = int(time_)
+    # print(time_)
+    time_ = int_to_bytestring(time_)
 
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-	params["aqi"] = aqi
-	params["days"] = days
-	params["alerts"] = alerts
+    key = key.encode()
+    key = base64.b32decode(key)
 
-	req = requests.get(url, params=params)
+    res = hmac.digest(key, time_, "sha1")
+    # print(res)
 
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
-#
-#
-#
-def history(req_format="json", location="Lisbon", date=time.strftime("%Y-%m-%d")):
-	req_type = "history"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
+    digits = 6
+    offset = res[-1] & 0xf
+    code = ((res[offset] & 0x7f) << 24 |
+            (res[offset + 1] & 0xff) << 16 |
+            (res[offset + 2] & 0xff) << 8 |
+            (res[offset + 3] & 0xff))
+    str_code = str(code % 10 ** digits)
+    while len(str_code) < digits:
+        str_code = '0' + str_code
 
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-	params["dt"] = date
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
-#
-#
-#
-def timezone(req_format="json", location="Lisbon"):
-	req_type = "timezone"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
-
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
-#
-#
-#
-def sports(req_format="json", location="Lisbon"):
-	req_type = "sports"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
-
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
-#
-#
-#
-def astronomy(req_format="json", location="Lisbon", date=time.strftime("%Y-%m-%d")):
-	req_type = "astronomy"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
-
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-	params["dt"] = date
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
-#
-#
-#
-def ip(req_format="json", location="Lisbon"):
-	req_type = "ip"
-	url = "{}{}.{}".format(URL_BASE, req_type, req_format)
-
-	params = dict()
-	params["key"] = API_KEY
-	params["q"] = location
-
-	req = requests.get(url, params=params)
-
-	resp = req.json()
-	# print(json.dumps(resp, indent=4))
-	return resp
+    print(str_code)
+    return str_code
 #
 #
 #
 if __name__ == "__main__":
-	location = "Sardinia"
-
-	date = time.strftime("%Y-%m-%d")
-	w = current(location=location)
-	print(json.dumps(w, indent=4))
-
-	w = forecast(location=location)
-	# print(json.dumps(w, indent=4))
-
-	# w = history(location="Lisbon", date="2020-05-26")
-	# w = timezone(location="Lisbon")
-	# w = sports()
-	# w = astronomy(location="Lisbon")
-	# w = ip(location="31.22.151.193")
-	# print(json.dumps(w, indent=4))
+    key = "".upper()
+    totp(key)
 #
 #
 #
